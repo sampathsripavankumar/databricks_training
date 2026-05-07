a0034485835f87bd35682ed3524b267af078babc
sampathsripavankumar <sampathsripavankumar@gmail.com>
2026-05-07 12:50:57 +0530

    Update queries.sql

--- week-1/day-1/queries.sql
+++ week-1/day-1/queries.sql
@@ -1,1 +1,162 @@
-1.select * from employee
+1.SELECT * FROM Employee;
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe    | 28  | 50000.0 | 1             | 2020-01-15 |
+| 2      | Jane Smith  | 34  | 60000.0 | 2             | 2019-07-23 |
+| 3      | Bob Brown   | 45  | 80000.0 | 1             | 2018-02-12 |
+| 4      | Alice Blue  | 25  | 45000.0 | 3             | 2021-03-22 |
+| 5      | Charlie P.  | 29  | 50000.0 | 2             | 2019-12-01 |
+| 6      | David Green | 38  | 70000.0 | 4             | 2022-05-18 |
+| 7      | Eve Black   | 40  | 55000.0 | 3             | 2021-08-30 |
+| 8      | Frank White | 32  | 48000.0 |               | 2021-07-10 |
+| 9      | Grace Kelly | 27  | 65000.0 | 1             | 2018-11-13 |
+| 10     | Hannah Lee  | 30  | 53000.0 | 4             | 2020-02-25 |
+
+2.SELECT name, salary
+FROM Employee;
+| name        | salary  |
+| ----------- | ------- |
+| John Doe    | 50000.0 |
+| Jane Smith  | 60000.0 |
+| Bob Brown   | 80000.0 |
+| Alice Blue  | 45000.0 |
+| Charlie P.  | 50000.0 |
+| David Green | 70000.0 |
+| Eve Black   | 55000.0 |
+| Frank White | 48000.0 |
+| Grace Kelly | 65000.0 |
+| Hannah Lee  | 53000.0 |
+
+3.SELECT *
+  FROM Employee
+  WHERE age > 30;
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 2      | Jane Smith  | 34  | 60000.0 | 2             | 2019-07-23 |
+| 3      | Bob Brown   | 45  | 80000.0 | 1             | 2018-02-12 |
+| 6      | David Green | 38  | 70000.0 | 4             | 2022-05-18 |
+| 7      | Eve Black   | 40  | 55000.0 | 3             | 2021-08-30 |
+| 8      | Frank White | 32  | 48000.0 |               | 2021-07-10 |
+
+4. SELECT name
+   FROM Department;
+
+| name      |
+| --------- |
+| IT        |
+| HR        |
+| Finance   |
+| Marketing |
+
+5.  SELECT *
+    FROM Employee
+    WHERE department_id = 1;
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe    | 28  | 50000.0 | 1             | 2020-01-15 |
+| 3      | Bob Brown   | 45  | 80000.0 | 1             | 2018-02-12 |
+| 9      | Grace Kelly | 27  | 65000.0 | 1             | 2018-11-13 |\
+
+
+6.  SELECT *
+    FROM Employee
+    WHERE name LIKE 'J%';
+
+| emp_id | name       | age | salary  | department_id | hire_date  |
+| ------ | ---------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe   | 28  | 50000.0 | 1             | 2020-01-15 |
+| 2      | Jane Smith | 34  | 60000.0 | 2             | 2019-07-23 |
+
+
+7.  SELECT *
+    FROM Employee
+    WHERE name LIKE '%e';
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe    | 28  | 50000.0 | 1             | 2020-01-15 |
+| 4      | Alice Blue  | 25  | 45000.0 | 3             | 2021-03-22 |
+| 8      | Frank White | 32  | 48000.0 |               | 2021-07-10 |
+| 10     | Hannah Lee  | 30  | 53000.0 | 4             | 2020-02-25 |
+
+8.  SELECT *
+    FROM Employee
+    WHERE name LIKE '%a%';
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 2      | Jane Smith  | 34  | 60000.0 | 2             | 2019-07-23 |
+| 4      | Alice Blue  | 25  | 45000.0 | 3             | 2021-03-22 |
+| 5      | Charlie P.  | 29  | 50000.0 | 2             | 2019-12-01 |
+| 6      | David Green | 38  | 70000.0 | 4             | 2022-05-18 |
+| 7      | Eve Black   | 40  | 55000.0 | 3             | 2021-08-30 |
+| 8      | Frank White | 32  | 48000.0 |               | 2021-07-10 |
+| 9      | Grace Kelly | 27  | 65000.0 | 1             | 2018-11-13 |
+| 10     | Hannah Lee  | 30  | 53000.0 | 4             | 2020-02-25 |
+
+9.  SELECT *
+    FROM Employee
+    WHERE name LIKE '_________';
+
+| emp_id | name      | age | salary  | department_id | hire_date  |
+| ------ | --------- | --- | ------- | ------------- | ---------- |
+| 3      | Bob Brown | 45  | 80000.0 | 1             | 2018-02-12 |
+| 7      | Eve Black | 40  | 55000.0 | 3             | 2021-08-30 |
+
+10. SELECT *
+    FROM Employee
+    WHERE name LIKE '_a%';
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 2      | Jane Smith  | 34  | 60000.0 | 2             | 2019-07-23 |
+| 6      | David Green | 38  | 70000.0 | 4             | 2022-05-18 |
+| 10     | Hannah Lee  | 30  | 53000.0 | 4             | 2020-02-25 |
+
+
+11.  SELECT *
+     FROM Employee
+     WHERE YEAR(hire_date) = 2020;
+
+| emp_id | name       | age | salary  | department_id | hire_date  |
+| ------ | ---------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe   | 28  | 50000.0 | 1             | 2020-01-15 |
+| 10     | Hannah Lee | 30  | 53000.0 | 4             | 2020-02-25 |
+
+12. SELECT *
+    FROM Employee
+    WHERE MONTH(hire_date) = 1;
+
+| emp_id | name     | age | salary  | department_id | hire_date  |
+| ------ | -------- | --- | ------- | ------------- | ---------- |
+| 1      | John Doe | 28  | 50000.0 | 1             | 2020-01-15 |
+
+13. SELECT *
+    FROM Employee
+    WHERE hire_date < '2019-01-01';
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 3      | Bob Brown   | 45  | 80000.0 | 1             | 2018-02-12 |
+| 9      | Grace Kelly | 27  | 65000.0 | 1             | 2018-11-13 |
+
+
+14. SELECT *
+    FROM Employee
+    WHERE hire_date >= '2021-03-01';
+
+| emp_id | name        | age | salary  | department_id | hire_date  |
+| ------ | ----------- | --- | ------- | ------------- | ---------- |
+| 4      | Alice Blue  | 25  | 45000.0 | 3             | 2021-03-22 |
+| 6      | David Green | 38  | 70000.0 | 4             | 2022-05-18 |
+| 7      | Eve Black   | 40  | 55000.0 | 3             | 2021-08-30 |
+| 8      | Frank White | 32  | 48000.0 |               | 2021-07-10 |
+
+15. SELECT *
+    FROM Employee
+    WHERE hire_date >= DATE_SUB(CURDATE(), INTERVAL 2 YEAR);
+
+There are no results to be displayed.
+
